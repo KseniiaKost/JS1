23700ab355706dac4ea61f55cad78712e91166ae
KseniiaKost <KKostyuchenko18@gmail.com>
2023-05-10 22:33:46 +0100

    HW4

---
+++ HW/HW4.js
@@ -0,0 +1,62 @@
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+//
+// function rectangular(a,b) {
+//     let result = a * b;
+//     return result
+// }
+//
+// console.log('area of a rectangle is', rectangular(100, 200));
+
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+//
+// function circle(r) {
+//     let result = 3.14 * r**2;
+//     return result
+// }
+// console.log('area of a circle is', circle(100))
+
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+// function cylinder(h, r) {
+//     if (h < 0 || r < 0) {
+//         console.log('error');
+//         return;
+//     }
+//     let result = 2 * 3.14 * r * h;
+//     return result;
+// }
+// console.log('area of a cylinder is', cylinder(100, -10));
+
+// - створити функцію яка приймає масив та виводить кожен його елемент
+// function printElements (...arr) {
+//     for (let arrElement of arr) {
+//         console.log(arrElement)
+//     }
+//
+// }
+// let x= [11,22,33,55,66,]
+// let y = [55,68,963,250]
+// printElements(x, y)
+
+// - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+// function p(textContent) {
+//     document.write (`<div>`)
+//     document.write (`<p> ${textContent} </p>`)
+//     document.write (`</div>`)
+// }
+// p('Text Content')
+// p('text')
+// - створити функцію яка створює ul
+// з трьома елементами li. Текст li задати через аргумент всім однаковий
+// function ul(text) {
+//     let content = 'Same Text'
+//     document.write (`<div>`)
+//     document.write (`<ul>
+// <li>${content}</li>
+// <li>${content}</li>
+// <li>${content}</li>
+// </ul>`)
+//     document.write (`</div>`)
+// }
+//  ul()
+
+
